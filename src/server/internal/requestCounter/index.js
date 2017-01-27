d97606e79755e57444449f16d64a4bf95839a7b3
Márton Borlay <borlaym@gmail.com>
2017-01-27 14:38:50 +0100

    feat(server/internal/requestCounter): Implement requestCounter methods

--- src/server/internal/requestCounter/index.js
+++ src/server/internal/requestCounter/index.js
@@ -1,9 +1,14 @@
-function RequestCounter() {
+function RequestCounter(container) {
 	return {
-		registerIncomingRequest: (url, param, time) => {},
-		getStatistic: () => {
+		registerIncomingRequest: (url, param, time) => {
+			const cache = container.get('cache');
+			cache.increment('totalIncomingRequests', 1);
+		},
+		getStatistic: async () => {
+			const cache = container.get('cache');
+			const totalIncomingRequests = await cache.get('totalIncomingRequests');
 			return {
-				totalIncomingRequests: 0
+				totalIncomingRequests
 			}
 		}
 	}
